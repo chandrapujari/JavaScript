6c3ba13d5f05802ef020d7b2c06069f0cf385a18
Chandra Shekhar Pujari <pujarichandrashekhar90@gmail.com>
2023-08-25 10:01:29 +0000

    data types

---
+++ 01_JSBasics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3)//we are using nodejs, not browser
+let Name="chandra"
+let Email="chandrapujari@gail.com"
+let Age="28"
+let Islogin=true
+let State;
+console.log(typeof null)
+console.log(typeof undefined)

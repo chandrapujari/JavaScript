441853723f060c5aa1528e6d95ea540a1feb3b1a
Chandra Shekhar Pujari <pujarichandrashekhar90@gmail.com>
2023-08-25 06:19:53 +0000

    understood behaviour of let,var,const

---
+++ 01_JSBasics/01_Variable.js
@@ -0,0 +1,12 @@
+const accountId=7345678
+let accontEmail="pujarichandra90@gmail.com"
+var accountPassword="12345"
+accountCity="Delhi"
+let accounyState;
+// CHECK variable values get change
+// accountId=09786
+accontEmail="chandrapujari90@gmail.com"
+accountPassword="09876543"
+accountCity="Haldwani"
+console.table([accountId, accontEmail, accountPassword, accountCity,accounyState])
+// console.log(accountState)
